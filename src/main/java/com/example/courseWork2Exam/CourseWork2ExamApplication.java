750c1f74ac524e978a4b59731fafe34aeae5c68f
Тарас <Acroyd1984@gmail.com>
2023-08-15 09:49:35 +0300

    Start

---
+++ src/main/java/com/example/courseWork2Exam/CourseWork2ExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.courseWork2Exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWork2ExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWork2ExamApplication.class, args);
+	}
+
+}
